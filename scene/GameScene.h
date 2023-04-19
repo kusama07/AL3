a5efb176effa6a7fba2e67e619056fdc36970c87
k022g0034@g.neec.ac.jp <k022g0034@g.neec.ac.jp>
2023-04-19 15:59:49 +0900

    01_01課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,6 +8,7 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -44,6 +45,24 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+	uint32_t textureHandle_ = 0;
+	//スプライト
+	Sprite* sprite_ = nullptr;
+	//3Dモデル
+	Model* model_ = nullptr;
+	//ワールドトランスフォーム
+	WorldTransform worldTransfrom_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+	//サウンドデータハンドル
+	uint32_t soudDateHandle_ = 0;
+	//音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+	//ImGuiで値を入力する変数
+	float inputFloat3[3] = {0, 0, 0};
+	//でバックカメラ
+	DebugCamera* debugCamera_ = nullptr;
+
 
 	/// <summary>
 	/// ゲームシーン用
