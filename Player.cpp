eb94ec270aa3ff71ba19382b0e6ec2fd9c52d7fe
k022g0034@g.neec.ac.jp <k022g0034@g.neec.ac.jp>
2023-04-19 17:11:21 +0900

    02_01課題

---
+++ Player.cpp
@@ -0,0 +1,18 @@
+﻿#include "Player.h"
+#include <cassert>
+
+void Player::Initialize(Model* model, uint32_t textureHandle) {
+	//NULLポインタチェック
+	assert(model);
+
+	worldTransform_.Initialize();
+
+}
+
+void Player::Update() {
+
+}
+
+void Player::Draw() {
+
+}
