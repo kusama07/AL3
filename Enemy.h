2cf172cc7ca51de1fcc76f67c56e0b1eecfa2b5a
k022g0034@g.neec.ac.jp <k022g0034@g.neec.ac.jp>
2023-05-17 14:41:59 +0900

    02_05

---
+++ Enemy.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "WorldTransform.h"
+#include "model.h"
+
+class Enemy {
+public:
+	void Initialize(Model* model, uint32_t textureHandle);
+
+	void Update();
+
+	void Draw(ViewProjection viewProjection);
+
+	~Enemy();
+
+private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+};
