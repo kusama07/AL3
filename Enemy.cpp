51b939e7f5ad1896538f796d868b20647304404f
k022g0034@g.neec.ac.jp <k022g0034@g.neec.ac.jp>
2023-05-17 14:56:08 +0900

    02_06

---
+++ Enemy.cpp
@@ -0,0 +1,40 @@
+﻿#include "Enemy.h"
+#include "Matrix.h"
+#include <cassert>
+
+Enemy::~Enemy() { 
+}
+
+void Enemy::Initialize(Model* model, uint32_t textureHandle) { 
+	assert(model);
+
+	model_ = model;
+	textureHandle_ = textureHandle;
+
+	worldTransform_.Initialize(); 
+
+}
+
+void Enemy::Update() { 
+	worldTransform_.TransferMatrix(); 
+
+	// キャラクターの移動ベクトル
+	Vector3 move = {0, 0, 0};
+
+	// キャラクターの移動速さ
+	const float kCharacterSpeed = 0.2f;
+
+	move.z -= kCharacterSpeed;
+
+	// 座標移動(ベクトルの加算)
+	worldTransform_.translation_ = Add(worldTransform_.translation_, move);
+
+	// 行列更新
+	worldTransform_.matWorld_ = MakeAffineMatrix(
+	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+}
+
+void Enemy::Draw(ViewProjection viewProjection) {
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+
+}
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -5,6 +5,7 @@
 #include "Input.h"
 #include "Model.h"
 #include "Player.h"
+#include "Enemy.h"
 #include "SafeDelete.h"
 #include "Sprite.h"
 #include "ViewProjection.h"
@@ -54,6 +55,7 @@ private: // メンバ変数
 	ViewProjection viewProjection_;
 
 	Player* player_ = nullptr;
+	Enemy* enemy_ = nullptr;
 
 	bool isDebugCameraActive_ = false;
 	DebugCamera* debugCamera_ = nullptr;
